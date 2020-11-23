a4bcaa9acfb60dc89b6dc155b5e728e26ed67149
rex.xie <rex.xie@minewtech.com>
2020-11-23 09:53:20 +0800

    Initial commit

---
+++ Example/MTBeaconPlus/MTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTAppDelegate.h
+//  MTBeaconPlus
+//
+//  Created by xxdeer on 11/23/2020.
+//  Copyright (c) 2020 xxdeer. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/MTBeaconPlus/MTViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTViewController.h
+//  MTBeaconPlus
+//
+//  Created by xxdeer on 11/23/2020.
+//  Copyright (c) 2020 xxdeer. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MTViewController : UIViewController
+
+@end
